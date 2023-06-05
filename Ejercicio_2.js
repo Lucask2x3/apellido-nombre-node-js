7355330a79105e6e39db97f3e7f4be72f7c1a092
Lucask2x3 <135330006+Lucask2x3@users.noreply.github.com>
2023-06-05 08:07:29 -0300

    ejercicio-2-node-js
    ejercicio-2-node-js

---
+++ Ejercicio_2.js
@@ -0,0 +1,5 @@
+const prompt=require('prompt-sync')();
+var num1=parseInt(prompt("Ingrese el primer numero "));
+var num2=parseInt(prompt("Ingrese el segundo numero "));
+var resultado= num1+num2
+console.log("El resultado es: ",resultado)
